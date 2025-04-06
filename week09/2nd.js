37cf8b15a051f79ca48043a0acbe00626371f953
Nastya77-al <nastyrymsha03@gmail.com>
2025-04-06 19:13:37 +0300

    Update 2nd.js

--- week09/2nd.js
+++ week09/2nd.js
@@ -1,48 +1,52 @@
-import * as THREE from '../three/three.module.js';
-import { GLTFLoader } from '../three/GLTFLoader.js';
+import * as THREE from 'three'; // Оновлюємо імпорт, щоб використовувати importmap
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor(0xCBEFFF,1);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xCBEFFF, 1);
 
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(renderer.domElement);
 
-var lightOne=new THREE.AmbientLight(0xffffff, 0.5); 
+var lightOne = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(lightOne);
 
-var lightTwo=new THREE.PointLight(0xffffff, 0.5);
+var lightTwo = new THREE.PointLight(0xffffff, 0.5);
 scene.add(lightTwo);
 lightTwo.position.set(-1.5, 0, -1);
 
-const light3 = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-scene.add( light3 );
+const light3 = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+scene.add(light3);
 
 camera.position.z = 3;
 
-const progress = document.getElementById ("progress");
-
+const progress = document.getElementById("progress");
 
 const loader = new GLTFLoader();
 
-loader.load('https://threejs.org/examples/models/gltf/Duck/glTF/Duck.gltf', function (gltf) {
-    scene.add(gltf.scene);
-    gltf.scene.rotation.set(0, -Math.PI/2, 0);
-}, 
-function ( xhr ) {
-	progress.innerHTML =  ( xhr.loaded / xhr.total * 100 ) + '% loaded';
-	if (xhr.loaded == xhr.total)
-		progress.innerHTML = "";
-}, function ( error ) {
-	console.error( error );
-} );
-
+loader.load(
+    'https://threejs.org/examples/models/gltf/Duck/glTF/Duck.gltf', // Завантажуємо модель Duck
+    function (gltf) {
+        console.log(gltf);
+        scene.add(gltf.scene);
+        // Залишаємо поворот, який був у твоєму коді для dog.glb
+        gltf.scene.rotation.set(0, -Math.PI / 2, 0);
+    },
+    function (xhr) {
+        progress.innerHTML = (xhr.loaded / xhr.total * 100) + '% loaded';
+        if (xhr.loaded == xhr.total)
+            progress.innerHTML = "";
+    },
+    function (error) {
+        console.error(error);
+    }
+);
 
 function animate() {
-	requestAnimationFrame(animate);
-	renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 }
 
 animate();
