4dfa34664e75f50afa3dfb4147169fd5d41bab93
Nastya77-al <nastyrymsha03@gmail.com>
2025-04-07 00:52:04 +0300

    Update mymask.js

--- week11/mymask.js
+++ week11/mymask.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import { MindARThree } from 'mindar-face-three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
 window.addEventListener("DOMContentLoaded", async () => {
@@ -25,26 +24,7 @@ window.addEventListener("DOMContentLoaded", async () => {
 	faceMesh.material.map = texture;
 	faceMesh.material.transparent = true;
 	faceMesh.material.needsUpdate = true;
-	//faceMesh.material.transparent = true;
 	scene.add(faceMesh);
-
-
-	/*const loaderGLTF = new GLTFLoader();
-	const anchorHat = mindarThree.addAnchor(10);
-	loaderGLTF.load( '../assets/police_hat.glb', function ( hat ) {
-		anchorHat.group.add(hat.scene);
-		console.log(hat);
-		hat.scene.scale.set(0.75, 0.75, 0.75);
-		hat.scene.rotation.set(0, -Math.PI/2, 0);
-		hat.scene.position.y -= 0.2;
-		hat.scene.position.z -= 0.2;
-	},
-		function ( xhr ) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded') ;
-	}, function ( error ) {
-		console.error( error );
-	} );*/
-
 
 	await mindarThree.start();
 
